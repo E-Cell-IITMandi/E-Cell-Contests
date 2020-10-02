97ffe9f455611a968b45ccb24d0d46fc0721e4e0
Signior-X <b19188@students.iitmandi.ac.in>
2020-10-03 02:14:58 +0530

    Initialised flutter with firestore for web only

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:ecell_register/AddUser.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ecell Register',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          // if (snapshot.hasError) {
+          //   return SomethingWentWrong();
+          // }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MyHomePage(
+              title: 'Hello World',
+            );
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return Text('data');
+        },
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: AddUser('Priyam', 'Seth', 15),
+      ),
+    );
+  }
+}
