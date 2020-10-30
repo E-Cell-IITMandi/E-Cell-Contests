c0fc339e7f344434227719842c91116192f979d2
Signior-X <b19188@students.iitmandi.ac.in>
2020-10-30 18:21:33 +0530

    Added about

--- lib/About.dart
+++ lib/About.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class About extends StatelessWidget {
   const About({Key key}) : super(key: key);
@@ -11,8 +12,45 @@ class About extends StatelessWidget {
         title: Text('About'),
       ),
       body: Center(
-        child: Text('About us'),
+        child: Container(
+          height: 600.0,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 16.0),
+                child: Text(
+                  'E-Cell IIT Mandi',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
+                child: RaisedButton(
+                  child: Text('Privacy Policy'),
+                  onPressed: () => _launchURL(
+                      'https://ecell.iitmandi.co.in/register/privacy-policy.html'),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 4.0),
+                child: RaisedButton(
+                  child: Text('Terms And Conditions'),
+                  onPressed: () => _launchURL(
+                      'https://ecell.iitmandi.co.in/register/terms-conditions.html'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
